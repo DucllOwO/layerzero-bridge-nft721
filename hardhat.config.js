16acdaee8aa3e929179d521d56130d6442f86989
nguyenduc0453 <nguyenduc0453@gmail.com>
2023-11-08 15:20:29 +0700

    init project

---
+++ hardhat.config.js
@@ -0,0 +1,116 @@
+require("dotenv").config()
+
+// uncomment to include contract sizing in test output
+// require("hardhat-contract-sizer")
+require("@nomiclabs/hardhat-waffle")
+require(`@nomiclabs/hardhat-etherscan`)
+require("solidity-coverage")
+// uncomment to include gas reporting in test output
+//require('hardhat-gas-reporter')
+require("hardhat-deploy")
+require("hardhat-deploy-ethers")
+require("@openzeppelin/hardhat-upgrades")
+require("./tasks")
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners()
+
+    for (const account of accounts) {
+        console.log(account.address)
+    }
+})
+
+function getMnemonic(networkName) {
+    if (networkName) {
+        const mnemonic = process.env["MNEMONIC_" + networkName.toUpperCase()]
+        if (mnemonic && mnemonic !== "") {
+            return mnemonic
+        }
+    }
+
+    const mnemonic = process.env.MNEMONIC
+    if (!mnemonic || mnemonic === "") {
+        return "test test test test test test test test test test test junk"
+    }
+
+    return mnemonic
+}
+
+function accounts(chainKey) {
+    return { mnemonic: getMnemonic(chainKey) }
+}
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.4",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+            {
+                version: "0.7.6",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+            {
+                version: "0.8.12",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+        ],
+    },
+
+    // solidity: "0.8.4",
+    contractSizer: {
+        alphaSort: false,
+        runOnCompile: true,
+        disambiguatePaths: false,
+    },
+
+    namedAccounts: {
+        deployer: {
+            default: 0, // wallet address 0, of the mnemonic in .env
+        },
+        proxyOwner: {
+            default: 1,
+        },
+    },
+
+    mocha: {
+        timeout: 100000000,
+    },
+
+    networks: {
+        "bsc-testnet": {
+            url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+            chainId: 97,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+        mumbai: {
+            url: "https://rpc-mumbai.maticvigil.com/",
+            chainId: 80001,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+    },
+}
